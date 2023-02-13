1d757e1f860459139f9f7f1180bc3e5237b01726
Your Name <you@example.com>
2023-02-13 23:21:47 +0700

    update is

--- src/comparison/helper.c
+++ src/comparison/helper.c
@@ -5,14 +5,24 @@
  * Обертка для ф _s21_is
  * Переворачивает аргументы если степени(exp) разные и вызывает функцию _s21_is
  *  @return int
- *          > 0 -> a > b
- *          = 0 -> a == b
- *          < 0 -> a < b
+ *          > 0 -> a > b  1
+ *          = 0 -> a == b 0
+ *          < 0 -> a < b  -1
+ * + = 0 , - = 1;
+   a b  res
+ / - -  !( a ? b )
+ / + -  a > b
+ / + +  a ? b
+ / - +  a < b
  */
 int s21_is(s21_decimal a, s21_decimal b, int *err) {
-  *err = normolize(s21_decimal *v1, s21_decimal *v2);
-  s21_decimal res = binary_subtraction(v1,v2,err);
-  if (!*err) int date = res.data[2] | res.data[1] | res.data[0];
-  if (res.head.sign) date *= -1;
+  int res = 1;
+  short revert = 0;
+  short sign = a.head.sign << 1 | b.head.sign;
+  if (sing == 0b11 && sing == 0b00) {
+    *err = dec_normalize(*a, *b);
+    res = compare(s21_decimal a, s21_decimal b);
+  }
+  if (sing == 0b11 & sing == 0b10) date != date;
   return date;
 }
