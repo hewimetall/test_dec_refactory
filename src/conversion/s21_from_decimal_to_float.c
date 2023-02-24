af26b376d14f5d8c9172281bd42ac3fe4b64c07d
Usolcev Vitaliy <usolcevvitaliy@gmail.com>
2023-02-24 15:52:49 +0700

    fix from dec to float

--- src/conversion/s21_from_decimal_to_float.c
+++ src/conversion/s21_from_decimal_to_float.c
@@ -2,7 +2,7 @@
 
 int s21_from_decimal_to_float(s21_decimal src, float *dst) {
   *dst = 0.f;
-  int status = CONVERTATION_OK;
+  int status = CONVERTATION_OK, sign = src.head.sign;
   if (s21_is_correct_decimal(src)) {
     if (!dst)
       status = CONVERTATION_ERROR;
@@ -12,7 +12,7 @@ int s21_from_decimal_to_float(s21_decimal src, float *dst) {
 
       while (src.head.exp--) tmp /= 10.0;
 
-      if (src.head.sign) tmp *= -1.0;
+      if (sign) tmp *= -1.0;
 
       *dst = tmp;
     }
